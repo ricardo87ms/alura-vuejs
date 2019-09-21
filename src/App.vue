ca907141cd39cef957a87344e3b558cd44e89663
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2019-09-21 08:27:22 -0300

    Criando componentes e encapsulamento de view

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
-    <ul>
-      <li v-for="(foto, index) in fotos" :key="index"><img :src="foto.url" :alt="foto.titulo"></li>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="(foto, index) in fotos" :key="index">
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel';
+
 export default {
+  components: {
+    'meu-painel': Painel
+  },
+
   data() {
     return {
       titulo: "Alura Pic",
@@ -25,4 +35,21 @@ export default {
 </script>
 
 <style>
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+  .centralizado {
+    text-align: center;
+  }
+  .lista-fotos {
+    list-style: none;
+  }
+  .lista-fotos-item {
+    display: inline-block;
+  }
+  .imagem-responsiva {
+    width: 100%;
+  }
 </style>
